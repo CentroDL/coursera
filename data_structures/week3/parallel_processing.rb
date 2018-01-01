2e823793e71f01ecd695a83d8d4c57f64aafc8cb
Dennis Liaw <dennis.liaw@gmail.com>
2017-12-31 22:26:36 -0500

    prioritizes thread number for priority queue hw

--- data_structures/week3/parallel_processing.rb
+++ data_structures/week3/parallel_processing.rb
@@ -1,39 +1,44 @@
-# require 'pry'
 def assign_jobs(worker_count, job_count, times)
-	workers = worker_count.times.map { |w| { thread: w, time: 0 } }
-	# binding.pry
-	queue = times.map.with_index do |job, i|
-		w = workers.first.dup
-		workers.first[:time] += job
-		sift_down(0, workers)
-		# binding.pry
-		w
-	end
+  workers = worker_count.times.map { |w| { thread: w, time: 0 } }
+  queue = times.map.with_index do |job, i|
+    w = workers.first.dup
+    workers.first[:time] += job
+    sift_down(0, workers)
+    w
+  end
 
-	queue.each do |job|
-		puts "#{job[:thread]} #{job[:time]}"
-	end
+  queue.each do |job|
+    puts "#{job[:thread]} #{job[:time]}"
+  end
 end
 
 # this needs to not only sift down if time is less but prioritize the indices of workers
 def sift_down(i, arr)
-	min = i
+  min = i
 
-	left_id  = (2*i) + 1
-	if left_id <= arr.size-1 && arr[left_id][:time] < arr[min][:time]
-		min = left_id
-	end
+  left_id  = (2*i) + 1
+  if left_id <= arr.size-1
+    if arr[left_id][:time] < arr[min][:time]
+      min = left_id
+    elsif arr[left_id][:time] == arr[min][:time] && arr[left_id][:thread] < arr[min][:thread]
+      min = left_id
+    end
+  end
 
-	right_id = (2*i) + 2 
-	if right_id <= arr.size-1 && arr[right_id][:time] < arr[min][:time]
-		min = right_id
-	end
+  right_id = (2*i) + 2
+  if right_id <= arr.size-1
+    if arr[right_id][:time] < arr[min][:time]
+      min = right_id
+    elsif arr[right_id][:time] == arr[min][:time] && arr[right_id][:thread] < arr[min][:thread]
+      min = right_id
+    end
+  end
 
-	if i != min
-		arr[i], arr[min] = arr[min], arr[i]
+  if i != min
+    arr[i], arr[min] = arr[min], arr[i]
 
-		sift_down(min, arr)
-	end
+    sift_down(min, arr)
+  end
 end
 
 # 1 <+ n <= 10^5, 1 <= m <= 10^5
