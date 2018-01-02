23e2e91e0437d79a51b159155435d1ca511caa54
Dennis Liaw <dennis.liaw@gmail.com>
2018-01-02 06:32:29 -0500

    turns out you need rank heuristics for speed

--- data_structures/week3/merging_tables.rb
+++ data_structures/week3/merging_tables.rb
@@ -1,60 +1,86 @@
+# require 'pry'
 class TableMerger
-  attr_reader :tables, :merges, :parents, :largests
+  attr_reader :tables, :merges, :parents, :pages, :ranks
+  attr_accessor :max_pages
 
-  def initialize(tables: tables, merges: merges, row_counts: row_counts)
+  def initialize(tables:, merges:, row_counts:)
     @tables     = tables
     @merges     = merges
     @parents    = []
-    @largests   = row_counts
+    @ranks      = []
+    @pages      = row_counts # pages in each table or set
+    @max_pages  = pages.max
   end
 
   def process_requests
-    m.times do |query|
+    merges.times do |query|
       destination, source = process_input
-      union( destination, source )
-    end
+      count = union( destination-1, source-1 )
 
-    # output page count
+      # binding.pry
+      if count > self.max_pages
+        self.max_pages = count
+      end
+      puts max_pages 
+    end
   end
 
+  # gives id of the base table
+  # have to compress the path to save time
   def find(i)
-    while i != parents[i]
-      i = parents[i]
+    parents[i] = i if parents[i].nil?
+
+    if i != parents[i]
+      parents[i] = find(parents[i])
     end
 
-    i
+    parents[i]
   end
 
-  def make_set(i)
-    parent[i] = i
-    largest[i]   = 0
-  end
+  # def make_set(i)
+  #   parent[i] = i
+  #   pages[i]  = 0
+  # end
 
   def union(destination, source)
     d = find(destination)
     s = find(source)
 
+    ranks[d] = 0 if ranks[d].nil?
+    ranks[s] = 0 if ranks[s].nil?
+
     if d == s
-      return
-    elsif d > s
+      return pages[d]
+    end
+
+    if ranks[d] > ranks[s]
+      parents[s] = d
+      pages[d] += pages[s]
+      pages[s] = 0
+
+      return pages[d]
+    else
+      # add source pages to destination and clear out source pages
+      parents[d] = s
+      pages[s] +=  pages[d]
+      pages[d] = 0
+
+      if ranks[s] == ranks[d]
+        ranks[s] += 1
+      end
+
+      return pages[s]
     end
   end
 
   def process_input
     gets.split(' ').map(&:to_i)
   end
-
-  def resolve_symlink
-  end
-
 end
 
-n, m = process_input
-rows = process_input
+n, m = gets.split(' ').map(&:to_i)
+rows = gets.split(' ').map(&:to_i)
 
 
 table_merger = TableMerger.new( tables: n, merges: m, row_counts: rows)
 table_merger.process_requests
-
-
-
