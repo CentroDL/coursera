8d37ef01bbd87faa94d293662ca50bd047e0ca46
Dennis Liaw <dennis.liaw@gmail.com>
2018-01-08 18:14:41 -0500

    solves fractional knapsack problem

--- algorithmic_toolbox/week3/fractional_knapsack.rb
+++ algorithmic_toolbox/week3/fractional_knapsack.rb
@@ -1,34 +1,36 @@
 # require 'pry'
 # output the optimal value output composed of fractional sums
 def calculate_max_value(num_items, capacity)
-	return 0 if capacity == 0
+  if capacity == 0
+    puts 0
+    return
+  end
 
-	items = []
-	value = 0
+  items = []
+  value = 0
 
-	num_items.times do |i|
-		item = {}
+  num_items.times do |i|
+    item = {}
+    item[:value], item[:weight] = gets.split(' ').map(&:to_f)
+    items <<  item
+  end
 
-		item[:value], item[:weight] = gets.split(' ').map(&:to_f)
-		items <<  item
-	end
+  #compute the value/weight ratios
+  items.each { |item| item[:ratio] = item[:value] / item[:weight] }
+  items.sort_by!{ |i| [ i[:ratio], -i[:weight] ] }.reverse!
 
-	#compute the value/weight ratios
-	items.each do |item|
-		item[:ratio] = item[:value] / item[:weight]
-	end
+  current_weight = 0
 
-	items.sort_by!{ |i| i[:ratio] }.reverse!
+  items.each do |item|
+    # with each iteration we've already changed the capacity so we roll
+    # with whatever's left
+    a = [item[:weight], capacity].min
+    # whatever you get, the ratio * weight will get you the value
+    value += a * item[:ratio]
+    capacity -= a
+  end
 
-	current_weight = 0
-
-	items.each do |item|
-		a = [item[:weight], capacity].min
-		value += a * item[:ratio]
-		capacity -= a
-	end
-
-	puts value.round(4)
+  puts value.round(4)
 end
 
 num_items, capacity = gets.split(' ').map(&:to_i)
